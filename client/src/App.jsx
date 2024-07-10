d21ece5b8d661660540143a3ed0cec9a361f1d2a
Mariam <marvellousmariam2006@gmail.com>
2024-07-10 23:12:03 +0100

    Fix my error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
 <Stack minH={"100vh"}>
 <Navbar/>
-<Container maxH={"100vh"} my={4}>
+<Container maxW={"1200px"} my={4}>
 <Text fontSize={{base:"3xl",md:"50"}} fontWeight={"bold"} letterSpacing={"2px"} textTransform={"uppercase"} textAlign={"center"} mb={8}>
 <Text as={"span"} bgGradient={"linear(to-r,cyan.400,blue.500)"} bgClip={"text"}>My Besties </Text>
  🚀
